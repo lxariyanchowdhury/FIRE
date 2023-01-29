d0e2d03b455a91278b72bd45de5b22287d416dd8
Lx Ariyan Chowdhury <103047016+Alexx-Shohan@users.noreply.github.com>
2023-01-29 11:44:41 +0600

    Update FIRE.py

--- FIRE.py
+++ FIRE.py
@@ -1,5 +1,19 @@
-import os, sys
-try:
-    __import__("FIRE1").menu()
-except Exception as e:
-    exit(str(e))
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import FIRE1.ariyan
+
+else:
+
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI •
+#---------------------------------------------------------#
